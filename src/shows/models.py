d29a3918f2e7ba3df8ae683b3c4ab62eb44c6f53
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-01-11 12:31:22 -0600

    Create show model and migrations

--- src/shows/models.py
+++ src/shows/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Show(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    created_at = models.DateTimeField(auto_now=True, null=False)
+    updated_at = models.DateTimeField(auto_now=True, null=False)
+    deleted_at = models.DateTimeField(auto_now=False, null=True)
+
+    class Meta:
+        ordering = ('created_at',)
