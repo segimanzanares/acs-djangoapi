3a6bdb203adde22324d0db9e416ba71b3ef08df2
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-01-14 22:37:35 -0600

    Fix exception handler

--- src/main/utils.py
+++ src/main/utils.py
@@ -5,24 +5,25 @@ def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
     response = exception_handler(exc, context)
-    if response.status_code == 400:
-        keys = list(response.data.keys())
-        response.data['error'] = {
-            'description': 'Some data were incorrect',
-            'code': 'invalid_request',
-            'fields': {}
-        }
-        for key in keys:
-            response.data['error']['fields'][key] = response.data[key]
-            response.data.pop(key, None)
-    else:
-        response.data['error'] = {
-            'description': exc.detail,
-            'code': exc.get_codes()
-        }
-        response.data.pop('detail', None)
-    # Now add the HTTP status code to the response.
-    if response is not None:
-        response.data['status_code'] = response.status_code
+    if ('status_code' in response):
+        if response.status_code == 400:
+            keys = list(response.data.keys())
+            response.data['error'] = {
+                'description': 'Some data were incorrect',
+                'code': 'invalid_request',
+                'fields': {}
+            }
+            for key in keys:
+                response.data['error']['fields'][key] = response.data[key]
+                response.data.pop(key, None)
+        else:
+            response.data['error'] = {
+                'description': exc.detail,
+                'code': exc.get_codes()
+            }
+            response.data.pop('detail', None)
+        # Now add the HTTP status code to the response.
+        if response is not None:
+            response.data['status_code'] = response.status_code
 
     return response
