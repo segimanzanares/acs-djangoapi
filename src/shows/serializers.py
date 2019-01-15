4cc79d48026b1d4cb1ad9fbc734ab8c2280ecb21
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2019-01-15 12:58:19 -0600

    Create episode serializer

--- src/shows/serializers.py
+++ src/shows/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
-from shows.models import Show
+from shows.models import Show, Episode
 
-class ShowSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True, allow_blank=False, max_length=100)
+class EpisodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Episode
+        fields = ('id', 'show', 'title', 'description', 'cover')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Episode` instance, given the validated data.
+        """
+        return Episode.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Show` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.save()
+        return instance
+
+class ShowSerializer(serializers.ModelSerializer):
+    episodes = EpisodeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Show
+        fields = ('id', 'title', 'episodes')
 
     def create(self, validated_data):
         """
